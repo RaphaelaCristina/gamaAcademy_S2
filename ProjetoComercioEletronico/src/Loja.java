0ef2e58da77f8963db527ddd8a20a07bd6c38be8
RaphaelaCristina <raphitasp@msn.com>
2022-09-12 22:32:41 -0300

    POO

--- ProjetoComercioEletronico/src/Loja.java
+++ ProjetoComercioEletronico/src/Loja.java
@@ -7,21 +7,21 @@ public class Loja {
     public static void main(String[] args) {
 
         Produto p, p2, p3; //declarei
-        p = new Produto(); //aloquei a memória para isso
-        p2 = new Produto(); //aloquei a memória para isso
-        p3= new Produto(); //aloquei a memória para isso
+        p = new Produto(123, "Computador" , 2500); //aloquei a memória para isso
+        p2 = new Produto(1234, "Mouse", 70.00); //aloquei a memória para isso
+        p3= new Produto(654, "Teclado", 120.00); //aloquei a memória para isso
 
-        p.codigo = 123;
-        p.descricao = "Computador";
-        p.preco = 2500.00;
-
-        p2.codigo = 1234;
-        p2.descricao = "Mouse";
-        p2.preco = 70.00;
-
-        p3.codigo = 654;
-        p3.descricao = "Teclado";
-        p3.preco = 120.00;
+//        p.setCodigo(123);
+//        p.setDescricao("Computador");
+//        p.setPreco(2500.00);
+//
+//        p2.setCodigo(1234);
+//        p2.setDescricao("Mouse");
+//        p2.setPreco(70.00);
+//
+//        p3.setCodigo(654);
+//        p3.setDescricao("Teclado");
+//        p3.setPreco(120.00);
 
         System.out.println("-------Preços antes dos descontos -------");
 
@@ -46,7 +46,7 @@ public class Loja {
 
         //Simulando descontos
         double precoP = p.simularDesconto(15);
-        System.out.println("O preço do " + p.descricao + " com mais 15% de desconto fica R$" + precoP);
+        System.out.println("O preço do " + p.getDescricao() + " com mais 15% de desconto fica R$" + precoP);
         p.mostrarAnuncio(); //O preço continua de 2250,00 pois o método apenas simulou e não aplicou o desconto
     }
 }
--- ProjetoComercioEletronico/src/Produto.java
+++ ProjetoComercioEletronico/src/Produto.java
@@ -3,25 +3,64 @@ import jdk.swing.interop.SwingInterOpUtils;
 //Definir meu novo tipo de dado que será usado por outras classes / programas java
 public class Produto {
 
-    int codigo;
-    String descricao;
-    double preco;
+    private int codigo;
+    private String descricao;
+    private double preco;
 
-     void mostrarAnuncio() {
+    //Construtor para criar o objeto passando os atributos obrigatoriamente como parametro.
+    public Produto(int codigo, String descricao, double preco){
+       this.codigo = codigo;
+       this.descricao = descricao;
+       this.preco = preco;
+    }
+
+    //Construtor selecionando quais atributos eu desejo que seja obrigatorio para criar o meu objeto
+    public Produto(int codigo, String descricao){
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = 0.00;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void mostrarAnuncio() {
         System.out.println("----------PRODUTO EM OFERTA----------");
         System.out.println(codigo + ":" + descricao);
         System.out.printf("R$ %.2f\n" , preco);
     }
 
-    void modificarPreco(double novoPreco){
-        this.preco = novoPreco;
+    public void modificarPreco(double novoPreco){
+       setPreco(novoPreco);
     }
 
-    void aplicarDesconto( double percentual){
-        this.preco = this.preco - (this.preco * percentual / 100);
+    public void aplicarDesconto( double percentual){
+        double preco = this.preco - (this.preco * percentual / 100);
+        setPreco(preco);
     }
 
-    double simularDesconto(double percentual){
+    public double simularDesconto(double percentual){
         double precoSimulado;
         precoSimulado = this.preco - (this.preco * percentual / 100);
         return precoSimulado;
