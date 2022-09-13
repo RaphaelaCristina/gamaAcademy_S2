7ee4eacf7db91bc402e00f1abceb90be76794124
RaphaelaCristina <raphitasp@msn.com>
2022-09-12 21:57:51 -0300

    Projeto Comercio Eletronico - POO

--- ProjetoComercioEletronico/src/Loja.java
+++ ProjetoComercioEletronico/src/Loja.java
@@ -6,15 +6,47 @@ public class Loja {
 
     public static void main(String[] args) {
 
-        Produto p; //declarei
+        Produto p, p2, p3; //declarei
         p = new Produto(); //aloquei a memória para isso
+        p2 = new Produto(); //aloquei a memória para isso
+        p3= new Produto(); //aloquei a memória para isso
 
         p.codigo = 123;
-        p.descricao = "computador";
+        p.descricao = "Computador";
         p.preco = 2500.00;
 
-        System.out.println("--------PRODUTO---------");
-        System.out.println(p.codigo + ":" + p.descricao );
-        System.out.printf("R$ %.2f\n" , p.preco);
+        p2.codigo = 1234;
+        p2.descricao = "Mouse";
+        p2.preco = 70.00;
+
+        p3.codigo = 654;
+        p3.descricao = "Teclado";
+        p3.preco = 120.00;
+
+        System.out.println("-------Preços antes dos descontos -------");
+
+        p.mostrarAnuncio();
+
+        p2.mostrarAnuncio();
+
+        p3.mostrarAnuncio();
+
+        System.out.println("*******************************************");
+
+        System.out.println("----PREÇOS DEPOIS DO DESCONTO-----");
+
+        p.aplicarDesconto(10);
+        p.mostrarAnuncio();
+
+        p2.modificarPreco(55);
+        p2.mostrarAnuncio();
+
+        p3.modificarPreco(99.90);
+        p3.mostrarAnuncio();
+
+        //Simulando descontos
+        double precoP = p.simularDesconto(15);
+        System.out.println("O preço do " + p.descricao + " com mais 15% de desconto fica R$" + precoP);
+        p.mostrarAnuncio(); //O preço continua de 2250,00 pois o método apenas simulou e não aplicou o desconto
     }
 }
--- ProjetoComercioEletronico/src/Produto.java
+++ ProjetoComercioEletronico/src/Produto.java
@@ -1,7 +1,29 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 //Definir meu novo tipo de dado que será usado por outras classes / programas java
 public class Produto {
 
     int codigo;
     String descricao;
     double preco;
+
+     void mostrarAnuncio() {
+        System.out.println("----------PRODUTO EM OFERTA----------");
+        System.out.println(codigo + ":" + descricao);
+        System.out.printf("R$ %.2f\n" , preco);
+    }
+
+    void modificarPreco(double novoPreco){
+        this.preco = novoPreco;
+    }
+
+    void aplicarDesconto( double percentual){
+        this.preco = this.preco - (this.preco * percentual / 100);
+    }
+
+    double simularDesconto(double percentual){
+        double precoSimulado;
+        precoSimulado = this.preco - (this.preco * percentual / 100);
+        return precoSimulado;
+    }
 }
